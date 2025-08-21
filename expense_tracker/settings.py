1fcfe12f69d1ed5b04834c7284f8033f50988d42
Harsh <harsh@example.com>
2025-08-21 18:21:33 +0530

    Fix deployment configuration: Add Procfile and update settings for Render deployment

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tracker/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
